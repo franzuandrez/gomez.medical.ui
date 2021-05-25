efddd22ef190e6ab577e27a0939a61e3774170e0
Franzua Andrez <edson.andrez@outlook.es>
2021-05-25 14:32:58 -0600

    gomezmedical.routes.js added.

---
+++ src/routes/gomezmedical.routes.js
@@ -0,0 +1,60 @@
+import { lazy } from 'react';
+import { Redirect } from 'react-router-dom';
+// guards
+import AuthGuard from '../guards/AuthGuard';
+// layouts
+import DashboardLayout from '../layouts/dashboard';
+//
+import { PATH_APP } from './paths';
+
+const GomezMedicalRoutes = {
+  path: PATH_APP.root,
+  layout: DashboardLayout,
+  routes: [
+    {
+      exact: true,
+      path: PATH_APP.locations.warehouses.root,
+      component: lazy(() => import('../views/gomezmedical/locations/warehouses'))
+    },
+    {
+      exact: true,
+      path: PATH_APP.locations.warehouses.newWarehouse,
+      component: lazy(() => import('../views/BlogPosts'))
+    },
+    {
+      exact: true,
+      path: PATH_APP.locations.sections,
+      component: lazy(() => import('../views/BlogPosts'))
+    },
+    {
+      exact: true,
+      path: PATH_APP.locations.corridors,
+      component: lazy(() => import('../views/UserProfile'))
+    },
+    {
+      exact: true,
+      path: PATH_APP.locations.racks,
+      component: lazy(() => import('../views/UserProfile'))
+    },
+    {
+      exact: true,
+      path: PATH_APP.locations.levels,
+      component: lazy(() => import('../views/UserProfile'))
+    },
+    {
+      exact: true,
+      path: PATH_APP.locations.positions,
+      component: lazy(() => import('../views/UserProfile'))
+    },
+    {
+      exact: true,
+      path: PATH_APP.locations.bines,
+      component: lazy(() => import('../views/UserProfile'))
+    },
+    {
+      component: () => <Redirect to='/404' />
+    }
+  ]
+};
+
+export default GomezMedicalRoutes;
