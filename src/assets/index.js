91b0e45a489cce26ff981f3e4dac2ba6160301f3
Franzua Andrez <edson.andrez@outlook.es>
2021-07-08 14:35:06 -0600

    New version of multiFile implemented.

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+
+export { default as UploadIllustration } from './illustration_upload';
+
+export { default as DocIcon } from './icon_doc';
--- src/components/upload/UploadMultiFile.js
+++ src/components/upload/UploadMultiFile.js
@@ -1,16 +1,18 @@
+import { isString } from 'lodash';
 import PropTypes from 'prop-types';
 import { Icon } from '@iconify/react';
-import { useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
 import fileFill from '@iconify/icons-eva/file-fill';
 import closeFill from '@iconify/icons-eva/close-fill';
 import { motion, AnimatePresence } from 'framer-motion';
 // material
-import { experimentalStyled as styled } from '@material-ui/core/styles';
+import { alpha, experimentalStyled as styled } from '@material-ui/core/styles';
 import {
   Box,
   List,
   Link,
+  Stack,
+  Paper,
   Button,
   ListItem,
   Typography,
@@ -37,64 +39,59 @@ const DropZoneStyle = styled('div')(({ theme }) => ({
   borderRadius: theme.shape.borderRadius,
   backgroundColor: theme.palette.background.neutral,
   border: `1px dashed ${theme.palette.grey[500_32]}`,
-  '&:hover': {
-    opacity: 0.72,
-    cursor: 'pointer'
-  },
-  [theme.breakpoints.up('md')]: {
-    textAlign: 'left',
-    flexDirection: 'row'
-  }
+  '&:hover': { opacity: 0.72, cursor: 'pointer' },
+  [theme.breakpoints.up('md')]: { textAlign: 'left', flexDirection: 'row' }
 }));
 
 // ----------------------------------------------------------------------
 
 UploadMultiFile.propTypes = {
-  caption: PropTypes.string,
   error: PropTypes.bool,
-  value: PropTypes.array,
-  onChange: PropTypes.func,
+  showPreview: PropTypes.bool,
+  files: PropTypes.array,
+  onRemove: PropTypes.func,
+  onRemoveAll: PropTypes.func,
   sx: PropTypes.object
 };
 
-export default function UploadMultiFile({
-  caption,
-  error = false,
-  value: files,
-  onChange: setFiles,
-  sx,
-  ...other
-}) {
+export default function UploadMultiFile({ error, showPreview = false, files, onRemove, onRemoveAll, sx, ...other }) {
   const hasFile = files.length > 0;
 
-  const handleDrop = useCallback(
-    (acceptedFiles) => {
-      setFiles(acceptedFiles);
-    },
-    [setFiles]
-  );
-
-  const handleRemoveAll = () => {
-    setFiles([]);
-  };
-
-  const handleRemoveFile = (file) => {
-    const newFiles = [...files];
-    newFiles.splice(newFiles.indexOf(file), 1);
-    setFiles(newFiles);
-  };
-
-  const {
-    getRootProps,
-    getInputProps,
-    isDragActive,
-    isDragReject
-  } = useDropzone({
-    onDrop: handleDrop
+  const { getRootProps, getInputProps, isDragActive, isDragReject, fileRejections } = useDropzone({
+    ...other
   });
 
+  const ShowRejectionItems = () => (
+    <Paper
+      variant="outlined"
+      sx={{
+        py: 1,
+        px: 2,
+        mt: 3,
+        borderColor: 'error.light',
+        bgcolor: (theme) => alpha(theme.palette.error.main, 0.08)
+      }}
+    >
+      {fileRejections.map(({ file, errors }) => {
+        const { path, size } = file;
+        return (
+          <Box key={path} sx={{ my: 1 }}>
+            <Typography variant="subtitle2" noWrap>
+              {path} - {fData(size)}
+            </Typography>
+            {errors.map((e) => (
+              <Typography key={e.code} variant="caption" component="p">
+                - {e.message}
+              </Typography>
+            ))}
+          </Box>
+        );
+      })}
+    </Paper>
+  );
+
   return (
-    <Box sx={{ width: '100%', ...sx }} {...other}>
+    <Box sx={{ width: '100%', ...sx }}>
       <DropZoneStyle
         {...getRootProps()}
         sx={{
@@ -108,87 +105,111 @@ export default function UploadMultiFile({
       >
         <input {...getInputProps()} />
 
-        <Box
-          component="img"
-          alt="select file"
-          src="/static/illustrations/illustration_upload.svg"
-          sx={{ height: 160 }}
-        />
 
-        <Box
-          sx={{
-            p: 3,
-            ml: { md: 2 }
-          }}
-        >
+        <Box sx={{ p: 3, ml: { md: 2 } }}>
           <Typography gutterBottom variant="h5">
             Drop or Select file
           </Typography>
 
-          {caption ? (
-            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-              {caption}
-            </Typography>
-          ) : (
-            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-              Drop files here or click&nbsp;
-              <Link underline="always">browse</Link>&nbsp;thorough your machine
-            </Typography>
-          )}
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            Drop files here or click&nbsp;
+            <Link underline="always">browse</Link>&nbsp;thorough your machine
+          </Typography>
         </Box>
       </DropZoneStyle>
 
-      <List disablePadding sx={{ ...(hasFile && { my: 5 }) }}>
+      {fileRejections.length > 0 && <ShowRejectionItems />}
+
+      <List disablePadding sx={{ ...(hasFile && { my: 3 }) }}>
         <AnimatePresence>
-          {files.map((file) => (
-            <ListItem
-              key={file.name}
-              component={motion.div}
-              {...varFadeInRight}
-              sx={{
-                my: 1,
-                py: 0.5,
-                px: 2,
-                borderRadius: 1,
-                border: (theme) => `solid 1px ${theme.palette.divider}`,
-                bgcolor: 'background.paper'
-              }}
-            >
-              <ListItemIcon>
-                <Icon icon={fileFill} width={32} height={32} />
-              </ListItemIcon>
-              <ListItemText
-                primary={file.name}
-                secondary={fData(file.size)}
-                primaryTypographyProps={{ variant: 'subtitle2' }}
-              />
-              <ListItemSecondaryAction>
-                <MIconButton
-                  edge="end"
-                  size="small"
-                  onClick={() => handleRemoveFile(file)}
+          {files.map((file) => {
+            const { name, size, preview } = file;
+            const key = isString(file) ? file : name;
+
+            if (showPreview) {
+              return (
+                <ListItem
+                  key={key}
+                  component={motion.div}
+                  {...varFadeInRight}
+                  sx={{
+                    p: 0,
+                    m: 0.5,
+                    width: 80,
+                    height: 80,
+                    borderRadius: 1.5,
+                    overflow: 'hidden',
+                    position: 'relative',
+                    display: 'inline-flex'
+                  }}
                 >
-                  <Icon icon={closeFill} />
-                </MIconButton>
-              </ListItemSecondaryAction>
-            </ListItem>
-          ))}
+                  <Paper
+                    variant="outlined"
+                    component="img"
+                    src={isString(file) ? file : preview}
+                    sx={{ width: '100%', height: '100%', objectFit: 'cover', position: 'absolute' }}
+                  />
+                  <Box sx={{ top: 6, right: 6, position: 'absolute' }}>
+                    <MIconButton
+                      size="small"
+                      onClick={() => onRemove(file)}
+                      sx={{
+                        p: '2px',
+                        color: 'common.white',
+                        bgcolor: (theme) => alpha(theme.palette.grey[900], 0.72),
+                        '&:hover': {
+                          bgcolor: (theme) => alpha(theme.palette.grey[900], 0.48)
+                        }
+                      }}
+                    >
+                      <Icon icon={closeFill} />
+                    </MIconButton>
+                  </Box>
+                </ListItem>
+              );
+            }
+
+            return (
+              <ListItem
+                key={key}
+                component={motion.div}
+                {...varFadeInRight}
+                sx={{
+                  my: 1,
+                  py: 0.75,
+                  px: 2,
+                  borderRadius: 1,
+                  border: (theme) => `solid 1px ${theme.palette.divider}`,
+                  bgcolor: 'background.paper'
+                }}
+              >
+                <ListItemIcon>
+                  <Icon icon={fileFill} width={28} height={28} />
+                </ListItemIcon>
+                <ListItemText
+                  primary={isString(file) ? file : name}
+                  secondary={isString(file) ? '' : fData(size)}
+                  primaryTypographyProps={{ variant: 'subtitle2' }}
+                  secondaryTypographyProps={{ variant: 'caption' }}
+                />
+                <ListItemSecondaryAction>
+                  <MIconButton edge="end" size="small" onClick={() => onRemove(file)}>
+                    <Icon icon={closeFill} />
+                  </MIconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            );
+          })}
         </AnimatePresence>
       </List>
 
       {hasFile && (
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'flex-end',
-            '& > *': { ml: 1.5 }
-          }}
-        >
-          <Button onClick={handleRemoveAll} sx={{ mr: 1.5 }}>
+        <Stack direction="row" justifyContent="flex-end">
+          <Button onClick={onRemoveAll} sx={{ mr: 1.5 }}>
             Remove all
           </Button>
           <Button variant="contained">Upload files</Button>
-        </Box>
+        </Stack>
       )}
     </Box>
   );
