430d1373b8b57df9520616f407fc5cca43f5651b
franzua <edson.andrez@outlook.es>
2022-01-18 19:38:46 -0600

    show Control Cash register header info

--- src/views/gomezmedical/cash_register/ControlCashRegisterShow.js
+++ src/views/gomezmedical/cash_register/ControlCashRegisterShow.js
@@ -3,8 +3,8 @@ import { Link as RouterLink, useParams } from 'react-router-dom';
 import {
   Box,
   Button,
-  Container,
-  Link
+  Container, Grid,
+  Link, Typography
 
 } from '@material-ui/core';
 import TableContainer from '@material-ui/core/TableContainer';
@@ -19,6 +19,7 @@ import Page from '../../../components/Page';
 import HeaderDashboard from '../../../components/HeaderDashboard';
 import apiControlCashRegister from '../../../services/api/cash_register/apiControlCashRegister';
 import { fCurrency } from '../../../utils/formatNumber';
+import Label from '../../../components/Label';
 
 
 export default function ControlCashRegisterStart() {
@@ -43,13 +44,48 @@ export default function ControlCashRegisterStart() {
           heading='Iniciar Control de caja'
           links={[
             { name: 'Listado', href: PATH_APP.cash_register_control.controls.root },
-            { name: 'Iniciar' }
+            { name: 'Ver' }
           ]}
 
         />
+        {!isLoading &&
+        <Grid container>
+          <Grid item xs={12} sm={6} sx={{ mb: 5 }}>
+            <Box sx={{ textAlign: { sm: 'left' } }}>
+              <Typography
+                paragraph
+                variant='overline'
+                sx={{ color: 'text.disabled' }}
+              >
+                General
+              </Typography>
+              <Typography variant='body2'>
+                Vendedor: {controlCashRegister.control.sales_person.person.first_name} {controlCashRegister.control.sales_person.person.last_name}
+              </Typography>
+              <Typography variant='body2'>
+                Turno: {controlCashRegister.control.shift.name}
+              </Typography>
 
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={6} sx={{ mb: 5 }}>
+            <Box sx={{ textAlign: { sm: 'right' } }}>
+              <Label
+                color={controlCashRegister.control.status === 'finalizado' ? 'success' : 'warning'}
+                sx={{ textTransform: 'uppercase', mb: 1 }}
+              >
+                {controlCashRegister.control.status}
+              </Label>
+              <Typography
+                variant='h6'>Caja: {controlCashRegister.control.cash_register.cash_register_number}</Typography>
+
+            </Box>
+
+          </Grid>
+
+        </Grid>
+        }
         <TableContainer component={Paper}>
-
           <Table sx={{ minWidth: 650 }} aria-label='simple table'>
             <TableHead>
               <TableRow>
@@ -103,25 +139,25 @@ export default function ControlCashRegisterStart() {
 
 
           </Table>
-          <Box sx={{ mt: 3, display: 'flex', justifyContent: 'flex-end' }}>
-
-
-            <Link
-              component={RouterLink}
-              to={`${PATH_APP.cash_register_control.controls.root}`}>
-              <Button
-                type='button'
-                color='inherit'
-                variant='outlined'
-                sx={{ mr: 1.5 }}
-              >
-                Regresar
-              </Button>
-            </Link>
-
-          </Box>
 
         </TableContainer>
+        <Box sx={{ mt: 3, display: 'flex', justifyContent: 'flex-end' }}>
+
+
+          <Link
+            component={RouterLink}
+            to={`${PATH_APP.cash_register_control.controls.root}`}>
+            <Button
+              type='button'
+              color='inherit'
+              variant='outlined'
+              sx={{ mr: 1.5 }}
+            >
+              Regresar
+            </Button>
+          </Link>
+
+        </Box>
       </Container>
     </Page>
   )
