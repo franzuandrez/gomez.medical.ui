5bb5f50507ca9ce5efe13f14c4551940c6190372
Franzua Andrez <edson.andrez@outlook.es>
2024-06-09 18:02:58 -0600

    cash as default

--- src/views/gomezmedical/sales/orders/SalesCheckoutPayment.js
+++ src/views/gomezmedical/sales/orders/SalesCheckoutPayment.js
@@ -66,7 +66,7 @@ export default function SalesCheckoutPayment({
   const formik = useFormik({
     initialValues: {
       delivery: shipping,
-      payment: '',
+      payment: 'cash',
       customer,
       amount_given: '',
       products: checkout
